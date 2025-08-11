2afd4476a9a6e1d0f8812c2fd71b96f88d1edd19
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2025-08-12 02:53:02 +0530

    Create lander.tsx

---
+++ src/components/lander/lander.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { HeroSection } from "./hero-section";
+
+export default function LandingPage() {
+  return (
+    <div className="container mx-auto">
+      <HeroSection />;
+    </div>
+  );
+}
