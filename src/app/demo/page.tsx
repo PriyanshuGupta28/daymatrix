401d4f1a27e380cd56aca47342726bcb7d398a02
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2025-08-12 19:12:30 +0530

    Update page.tsx

--- src/app/demo/page.tsx
+++ src/app/demo/page.tsx
@@ -1,4 +1,5 @@
 import CalendarView from "@/components/full-calendar/calendar-view";
+import MonthPlanner from "@/components/month-planner/month-planner";
 import { Metadata } from "next";
 export const metadata: Metadata = {
   title: "Demo | Daymatrix",
@@ -6,7 +7,13 @@ export const metadata: Metadata = {
 export default function Demo() {
   return (
     <>
-      <CalendarView />
+      <section className="container py-6 mx-auto">
+        <MonthPlanner />
+      </section>
+      <div className="mt-10 border-t py-5">
+        <p className="text-3xl text-center">Calendar using FullCalendar.io</p>
+        <CalendarView />
+      </div>
     </>
   );
 }
